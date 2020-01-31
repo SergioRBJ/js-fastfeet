d52423494b36f618e09a1094e06a92c5d1b87aa5
SergioRBJ <sergiorbj93@gmail.com>
2020-01-30 23:13:19 -0300

    first commit

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,134 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientControler {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      details: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zipcode: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipientExists = await Recipient.findOne({
+      where: { name: req.body.name },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient already exists' });
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      details,
+      state,
+      city,
+      zipcode,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      details,
+      state,
+      city,
+      zipcode,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.string(),
+      details: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zipcode: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id, name } = req.body;
+
+    const recipient = await Recipient.findByPk(req.body.id);
+
+    const recipientExists = await Recipient.findOne({
+      where: { name },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient already exists.' });
+    }
+
+    const {
+      street,
+      number,
+      details,
+      state,
+      city,
+      zipcode,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      details,
+      state,
+      city,
+      zipcode,
+    });
+  }
+
+  async destroy(req, res) {
+    const { id } = req.params;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res
+        .status(400)
+        .json({ error: 'The recipient ID does not exists.' });
+    }
+
+    await Recipient.destroy({ where: { id } });
+
+    return res.json(`The registration with id ${id} has been deleted.`);
+  }
+
+  async index(req, res) {
+    const recipient = await Recipient.findAll({
+      attributes: [
+        'id',
+        'name',
+        'number',
+        'street',
+        'state',
+        'details',
+        'city',
+        'zipcode',
+      ],
+    });
+
+    return res.status(200).json(recipient);
+  }
+}
+
+export default new RecipientControler();
---
+++ src/database/migrations/20200130172601-create-recipients.js
@@ -0,0 +1,52 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipients', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      details: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      zipcode: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        AllowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        AllowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('recipients');
+  },
+};
